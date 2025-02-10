fb0382d9a28ff732428b407a67d05d9d09cb03cb
bianders <phat@biden.co.uk>
2025-02-09 20:56:42 -0600

    WIP: mentor_agent script; serious refactor needed of ReACT class.

--- agentic/mentor_agent.py
+++ agentic/mentor_agent.py
@@ -1,6 +1,10 @@
-from Chain import ReACT, Model
+from Chain import ReACT, Model, Parser
 from Curator import Curate
-from functools import partial
+from Mentor import (
+    review_curriculum,
+    learner_progression,
+)
+from Kramer import Curation, Get
 
 
 # ReACT parameters
@@ -14,23 +18,46 @@ def find_similar_courses(topic: str) -> list[str]:
     """
     Ask the librarian for courses that are most similar to a search string.
     """
-    suggestions = Curate(topic)
+    suggestions = Curate(topic, n_results=50, k=15)
     suggestions = [suggestion[0] for suggestion in suggestions]
     return suggestions
 
 
-def get_second_opinion(topic: str) -> str:
+def get_second_opinion(curation_title: str, curation: list[str], audience: str) -> str:
     """
-    Ask an L&D admin to review the list of courses.
+    Ask an L&D admin to review the list of courses. Curation should be a list of strings.
     """
-    pass
+    courses = [Get(course) for course in curation]
+    if any(course is None for course in courses):
+        return (
+            "Some courses could not be found. Please provide a valid list of courses."
+        )
+    curation_object = Curation(title=curation_title, courses=courses)
+    review = review_curriculum(curation_object, audience)
+    return review
 
 
-def learner_review(topic: str) -> str:
+def learner_review(curation_title: str, curation: list[str], audience: str) -> str:
     """
-    Ask a learner to watch all the courses and provide feedback.
+    Ask a learner to watch all the courses and provide feedback. Curation should be a list of strings.
     """
-    pass
+    courses = [Get(course) for course in curation]
+    if any(course is None for course in courses):
+        return (
+            "Some courses could not be found. Please provide a valid list of courses."
+        )
+    curation_object = Curation(title=curation_title, courses=courses)
+    review = learner_progression(curation_object, audience)
+    return review
 
 
-# MentorAgent = ReACT(input, output, model)
+if __name__ == "__main__":
+    model = Model("o3-mini")
+    MentorAgent = ReACT(
+        input,
+        output,
+        tools=[find_similar_courses, get_second_opinion, learner_review],
+        model=model,
+        log_file="mentor_agent.log",
+    )
+    MentorAgent.query("Python for Business Analysts")
